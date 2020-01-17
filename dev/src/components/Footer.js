953dd89900f4f215e435168bd73fccaa6647511c
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-17 12:20:55 +0000

    Footer update

--- dev/src/components/Footer.js
+++ dev/src/components/Footer.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import gitLogoDark from '../images/GitHub-Mark-120px-plus.png';
-import gitLogoLight from '../images/GitHub-Mark-Light-120px-plus.png';
+import gitLogoDark from '../images/GitHub-Mark-64px.png';
+import gitLogoLight from '../images/GitHub-Mark-Light-64px.png';
 import './styles/Footer.css';
 
 export default function Footer() {
     return (
         <footer className="footer">
-            <a className="github-link"></a>
+            <a href="https://github.com/Chingu-Solo/solo-koala-54" className="github-link">
+                <div className="github-link__left-text">The GitHub Repo</div>
+                <div>
+                    <img src={gitLogoLight} className="github-logo--light" alt=""/>
+                    <img src={gitLogoDark} className="github-logo--dark" alt=""/>
+                </div>
+                <div className="github-link__right-text">Chingu Solo project</div>
+            </a>
         </footer>
     );
 }
