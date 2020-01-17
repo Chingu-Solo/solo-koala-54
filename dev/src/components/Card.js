9d9c008cfa26178e7d451c5aecfd56fa29e9b8df
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-17 16:35:01 +0000

    removed file fetching from Card and put into retrieveFontFileUrl

--- dev/src/components/Card.js
+++ dev/src/components/Card.js
@@ -1,40 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, css } from 'aphrodite/no-important';
-import axios from 'axios';
 import './styles/Card.css';
+import retrieveFontFileUrl from '../controllers/retrieveFontFileUrl';
 
 export default function Card(props) {
-    const [fontText, setfontText] = useState(false);
+    const [fontUrl, setFontUrl] = useState(false);
+    const name = props.family;
+    const googleFont = !fontUrl ? { fontFamily: 'inherit' } : {
+        fontFamily: props.family,
+        fontStyle: "normal",
+        fontWeight: "normal",
+        src: `url('${fontUrl}') format('truetype')`
+    };
+    const styles = StyleSheet.create({
+        font: {
+            fontFamily: [googleFont],
+        }
+    });
     useEffect(() => {
-        const varient = props.files.hasOwnProperty('regular') ? 'regular' : props.files.hasOwnProperty('400') ? '400' : Object.keys(props.files)[0]; // fallbacks
-        const url = props.files[varient]; 
-        const method = 'GET';
-        const name = props.family;
-        axios.request({url, method, responseType: 'blob'})
-        .then(({ data }) => {
-            const fontFile = new File([data], props.family+'.ttf');
-            const fontURL = URL.createObjectURL(fontFile);
-            const googleFont = {
-                fontFamily: props.family,
-                fontStyle: "normal",
-                fontWeight: "normal",
-                src: `url('${fontURL}') format('truetype')`
-            };
-            const styles = StyleSheet.create({
-                font: {
-                    fontFamily: [googleFont],
-                }
-            });
-            setfontText(<div className={'card '+css(styles.font)}> 
+        retrieveFontFileUrl(name, props.files).then(url => {
+            setFontUrl(url);
+          });        
+        }, [name, props.files]);
+    return (
+        <div className={'card '+css(styles.font)}> 
             <header className="card__heading" title={props.family}>
                 <span className="card__heading__title">{name}</span>
             </header>
-            <p className={'card__text '+css(styles.font)}>{props.customText}</p>
-        </div>);
-          });        
-        }, [props.customText, props.family, props.files]);
-    return (
-        fontText
+            {!fontUrl ? <p>Sorry: {props.family} file is not avalible</p> : <p className={'card__text '+css(styles.font)}>{props.customText}</p>}
+        </div>
     )
 } 
  
