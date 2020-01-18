91e3eff8372085d4179f0c2f392b2b257949777d
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-18 10:37:27 +0000

    Update buildSearchIndex.js
    Fix for empty fontList sometimes on first load

--- dev/src/controllers/buildSearchIndex.js
+++ dev/src/controllers/buildSearchIndex.js
@@ -1,4 +1,8 @@
 export default async function buildSearchIndex(fonts) {
+    if (!Array.isArray(fonts)) {
+        console.info('Empty fontList passed to buildSearchIndex');
+        return [];
+    }
     const fontNamesWithIndex = fonts.slice().map((font, i) => {
         return {name: font.family, index: i}
     });
