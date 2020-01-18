7abd69acd0457d17a21bf4ffe93223d1acd26f7c
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-18 16:43:51 +0000

    collection parts implimented on Card

--- dev/src/components/Card.js
+++ dev/src/components/Card.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, css } from 'aphrodite/no-important';
 import './styles/Card.css';
 import retrieveFontFileUrl from '../controllers/retrieveFontFileUrl';
+import removeIcon from '../images/removefromcollectionicon.svg';
+import addIcon from '../images/addtocollectionicon.svg';
 
 export default function Card(props) {
     const [fontUrl, setFontUrl] = useState(false);
@@ -17,6 +19,13 @@ export default function Card(props) {
             fontFamily: [googleFont],
         }
     });
+    const removeFromCollection = () => {
+        props.removeCardFromCollection(name);
+    }
+    const addToCollection = () => {
+        props.addCardToCollection(props);
+    }
+    const headerTitle = `${props.isInCollection ? 'Remove ' : 'Add '+name} ${props.isInCollection ? 'from' : 'to'} collection`;
     useEffect(() => {
         retrieveFontFileUrl(name, props.files).then(url => {
             setFontUrl(url);
@@ -24,11 +33,14 @@ export default function Card(props) {
         }, [name, props.files]);
     return (
         <div className={'card '+css(styles.font)}> 
-            <header className="card__heading" title={props.family}>
-                <span className="card__heading__title">{name}</span>
-            </header>
-            {!fontUrl ? <p>Sorry: {props.family} file is not avalible</p> : <p className={'card__text '+css(styles.font)}>{props.customText}</p>}
-        </div>
+                <header className="card__heading" title={headerTitle} onClick={props.isInCollection ? removeFromCollection : addToCollection}>
+                {props.isInCollection ? 
+                    <img className="card__collection-button card__collection-button--remove" src={removeIcon} alt="" /> : 
+                    <img className="card__collection-button card__collection-button--add" src={addIcon} alt="" />}
+                    <span className="card__heading__title">{name}</span>
+                </header>
+                {!fontUrl ? <p>Sorry: {props.family} file is not avalible</p> : <p className={'card__text '+css(styles.font)}>{props.customText}</p>}
+            </div>
     )
 } 
  
