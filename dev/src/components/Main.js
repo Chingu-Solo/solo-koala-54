88eeb1a3ad924fd5c68b8b52cc00061468788172
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-22 12:59:14 +0000

    removed consoleLogs

--- dev/src/components/Main.js
+++ dev/src/components/Main.js
@@ -53,7 +53,6 @@ export default function Main(props) {
     useEffect(() => {
         canResetPageBottom.current = true;
     });
-    console.log(collection)
     return (
         <main className={`catalog ${showCollection ? 'collection-open' : gridView ? 'grid' : 'bar'}`}>
             {showCollectionButton && <CollectionOpenButton list={collection.list} showCollection={() => setShowCollection(true)}/>}
