16b7181a435399350bfe2dce67f4c3e6a227817f
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-23 20:48:13 +0000

    Update CollectionOpenButton.js
    Made it so if it's the first time the page loads add there are fonts in the collection the collection button will fetch font files, else the fonts have already been view and it can just use the font family name

--- dev/src/components/CollectionOpenButton.js
+++ dev/src/components/CollectionOpenButton.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import wiggerlyLine from '../images/wiggerlyline.svg';
-import { css } from 'aphrodite/no-important';
+import { StyleSheet, css } from 'aphrodite/no-important';
+import retrieveFontFileUrl from '../controllers/retrieveFontFileUrl';
 import './styles/CollectionOpenButton.css';
 
 export default function CollectionOpenButton(props) {
+    const firstTimeLoading = useRef(true);
     const blankLines = () => {
         const arr = [];
         for (let l=props.list.length;l<3;l++) {
@@ -11,7 +13,33 @@ export default function CollectionOpenButton(props) {
         }
         return arr
     }
-    const fontsInCollection = () => props.list.map((font, i, arr) => (arr.length-i <= 3) ? <div key={font.family+'collectionList'} className={font.hasOwnProperty('styles') ? css(font.styles.fontHeading) : ''}>{i+1}. {font.family}</div> : false).filter(font => font);
+    const [fontUrls, setFontUrls] = useState({});
+    const styles = {};
+    props.list.forEach(font => {
+        if (firstTimeLoading.current) {
+            firstTimeLoading.current = false;
+            const googleFont = !fontUrls.hasOwnProperty(font.family) ? { fontFamily: 'inherit' } : {
+                fontFamily: font.family,
+                src: `url('${fontUrls[font.family]}') format('truetype')`
+            };
+            styles[font.family] = StyleSheet.create({font: {fontFamily: [googleFont]} });
+        }
+        else {
+            styles[font.family] = StyleSheet.create({font: {fontFamily: font.family} });
+        }
+    });
+    const fontsInCollection = () => props.list.map((font, i, arr) => (arr.length-i <= 3) ? <div key={font.family+'collectionList'} className={font.hasOwnProperty('styles') ? css(styles[font.family].font) : ''}>{i+1}. {font.family}</div> : false).filter(font => font);
+    useEffect(() => {
+        if (firstTimeLoading.current) {
+            const urls = {};
+            Promise.all(props.list.map(font => retrieveFontFileUrl(font.family, font.files)
+                    .then(url => {
+                        urls[font.family] = url;
+                    })
+                ))
+                .then(() => setFontUrls(urls));   
+        } 
+    }, [props.list]);
     return (
         <div className="collection-open-button" onClick={props.showCollection}>
             <h4 className="collection-open-button__title">Collection</h4>
