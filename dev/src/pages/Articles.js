9ab9318d70537b98056dc9dc85adac73f7471785
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-23 19:37:38 +0000

    Update Articles.js

--- dev/src/pages/Articles.js
+++ dev/src/pages/Articles.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Articles() {
     return (
         <main>
-            For articles go to <a href="https://design.google/library/google-fonts/?utm_source=Google&utm_medium=Fonts&utm_campaign=Article%20Tab" target="_blank">
+            For articles go to <a href="https://design.google/library/google-fonts/?utm_source=Google&utm_medium=Fonts&utm_campaign=Article%20Tab" target="_blank" rel="noopener noreferrer">
                 Google Design (external site)
             </a>
         </main>
