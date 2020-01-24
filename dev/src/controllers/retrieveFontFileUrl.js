b6933e5ce34eb8c266407812c50dc9f0d10da698
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-24 09:00:29 +0000

    Update retrieveFontFileUrl.js

--- dev/src/controllers/retrieveFontFileUrl.js
+++ dev/src/controllers/retrieveFontFileUrl.js
@@ -4,7 +4,7 @@ export default function retrieveFontFileUrl(name, files) {
     const varient = files.hasOwnProperty('regular') ? 'regular' : files.hasOwnProperty('400') ? '400' : Object.keys(files)[0]; // fallbacks
     let url = files[varient]; 
     const method = 'GET';
-    url = 'https'+url.substr(4);
+    url = 'https'+url.substr(4); // hack to replace http request
     return axios.request({url, method, responseType: 'blob'})
     .then(({ data }) => {
         const fontFile = new File([data], name+'.ttf');
