8da98639d0fdea224aaceaf1da53e8af70a04ce2
Lily Law <46561378+lily-law@users.noreply.github.com>
2020-01-28 19:17:09 +0000

    Reimplimented OnPageBottom

--- dev/src/controllers/OnPageBottom.js
+++ dev/src/controllers/OnPageBottom.js
@@ -1,43 +1,22 @@
-import React, { useLayoutEffect, useRef, useEffect } from 'react';
-import throttle from '../utils/throttle';
+import React from 'react';
+import handleViewport from 'react-in-viewport';
+import loading from '../images/loading.gif';
 
 export default function OnPageBottom(props) {
-    const controlPassedToFunction = useRef(false); // as a latch
-    let exec = props.exec || (() => console.info('No function given for OnPageBottom'));
-    const execScope = props.scope || null;
-    const callback = props.callback || false; 
-    const timeout = props.timeout || 500; // if passed in function won't callback use a timeout to reset latch
-    let checkPos = () => {
-        const pageHeight = document.body.scrollHeight;
-        const scrollPosFromPageBottom = window.pageYOffset + window.innerHeight;
-        const offset = window.innerHeight * ((props.offsetPercent || 25)/100);
-        if (!controlPassedToFunction.current && pageHeight - (scrollPosFromPageBottom + offset) <= 0) {
-            controlPassedToFunction.current = true;
-            new Promise((resolve, reject) => {
-                exec.apply(execScope, [...arguments, resolve, reject]);
-                if (callback) { 
-                    callback(resolve)
-                }
-                else {
-                 setTimeout(resolve, timeout);
-                }
-            }).then(shouldKeepChecking => {
-                controlPassedToFunction.current = false;
-                shouldKeepChecking && checkPos();
-            });
+    const { setNCards, nCards, isMoreCards } = props;
+    const PageBottomBlock = props => {
+        const { inViewport, forwardedRef } = props;
+        
+        if (isMoreCards()) {
+            if (inViewport) {
+                setNCards(nCards + 20);
+            } 
+            return <div ref={forwardedRef} className="page-bottom" ><img src={loading} alt="loading more..." /></div>
         }
-    }
-    const listingForScroll = useRef(false); // as a latch
-    useEffect(() => {
-        return () => controlPassedToFunction.current = true; // prevents exec being called after dismount
-    }, []);
-    useLayoutEffect(() => {
-        if (document && !listingForScroll.current) {
-            listingForScroll.current = true;
-            document.addEventListener('scroll', throttle(checkPos, 250));
-            checkPos(); // check intial loaded page
+        else {
+            return <div className="page-bottom">End of results</div>
         }
-        return () => document.removeEventListener('scroll', throttle(checkPos, 250));
-    })
-    return <></>
+    } 
+    let PageBottom = handleViewport(PageBottomBlock, { rootMargin: '20%' }, { disconnectOnLeave: true });
+    return <PageBottom />
 }
