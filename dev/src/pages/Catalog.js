166d856a01d57841de9035bb5eeb430e32e17ed0
Lily Law <46561378+lily-law@users.noreply.github.com>
2020-01-29 09:16:46 +0000

    Moved Toolbar into Catalog

--- dev/src/pages/Catalog.js
+++ dev/src/pages/Catalog.js
@@ -5,20 +5,13 @@ import {
     useRouteMatch
   } from "react-router-dom";
 import Main from '../components/Main';
+import Toolbar from '../components/Toolbar';
 
 function Catalog(props) {
   let match = useRouteMatch(); // using path for query so queries can be bookmarked
   const {fontList, searchIndex, gridView, customText, collection, fontSize} = props;
   return (<>
-      {props.toolbar}
-      <Switch>
-        <Route path={`${match.path}:filter`}>
-          <Main {...{fontList, searchIndex, gridView, customText, collection, fontSize}} />
-        </Route>
-        <Route path={match.path}>
-          <Main {...{fontList, searchIndex, gridView, customText, collection, fontSize}} />
-        </Route>
-      </Switch>
+        <Toolbar {...{setCustomText, customText, fontSize, setFontSize, gridView, setGridView, lightTheme, setLightTheme, setCollectionList}} />
     </>
   );
 }
