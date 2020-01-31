cedf5fa15472bd1eacbfb591cf6b44942dbce3aa
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-31 11:43:49 +0000

    Escape any regex modifiers before querying search

--- dev/src/controllers/querySearch.js
+++ dev/src/controllers/querySearch.js
@@ -1,7 +1,10 @@
+import escapeRegexMods from '../utils/escapeRegexMods';
+
 export default function querySearch(filter, fontList) {
-    const firstPass = fontList.filter(font => font.family.match(new RegExp(filter, 'iy'))).map(font => font.index); // Matches only cases starting with filter
-    const seconfPass = fontList.filter(font => font.family.match(new RegExp(filter, 'ig'))).map(font => font.index); // only cases that contain a word that start with filter
-    const thirdPass = fontList.filter(font => font.family.match(new RegExp(filter, 'i'))).map(font => font.index); // any cases that contain filter
+    const sanitisedFilter = escapeRegexMods(filter);
+    const firstPass = fontList.filter(font => font.family.match(new RegExp(sanitisedFilter, 'iy'))).map(font => font.index); // Matches only cases starting with filter
+    const seconfPass = fontList.filter(font => font.family.match(new RegExp(sanitisedFilter, 'ig'))).map(font => font.index); // only cases that contain a word that start with filter
+    const thirdPass = fontList.filter(font => font.family.match(new RegExp(sanitisedFilter, 'i'))).map(font => font.index); // any cases that contain filter
     const queryIndex = new Set();  
     [...firstPass, ...seconfPass, ...thirdPass].forEach(i => queryIndex.add(i));
     return [...queryIndex];
