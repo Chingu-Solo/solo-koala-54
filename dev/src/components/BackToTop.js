9fa8d96031fafd90ddd63a872e713b95fdbfec31
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-31 20:22:47 +0000

    BackToTop fades in

--- dev/src/components/BackToTop.js
+++ dev/src/components/BackToTop.js
@@ -2,6 +2,7 @@ import React, {useState, useRef, useLayoutEffect} from 'react';
 import icon from '../images/backToTop.svg';
 import './styles/BackToTop.css';
 import throttle from '../utils/throttle';
+import {useTransition, animated} from 'react-spring'
 
 export default function BackToTop() {
     const [showButton, setShowButton] = useState(false);
@@ -16,6 +17,11 @@ export default function BackToTop() {
         }
     }
     const listingForScroll = useRef(false); // as a latch
+    const transitions = useTransition(showButton, null, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        })
     useLayoutEffect(() => {
         if (document && !listingForScroll.current) {
             listingForScroll.current = true;
@@ -23,8 +29,8 @@ export default function BackToTop() {
         }
         return () => document.removeEventListener('scroll', throttle(() => checkPos(), 500));
     });
-    return (<>
-        {showButton ? <img className="back-to-top" src={icon} onClick={() => window.scrollTo(0, 0)} alt="Back to top" title="Back to top" />:
-        <></>}
-    </>)
+    return transitions.map(({ item, key, props }) =>
+        item && <animated.img style={props} key={key} className="back-to-top" src={icon} onClick={() => window.scrollTo(0, 0)} alt="Back to top" title="Back to top" />
+    )
+         
 }
